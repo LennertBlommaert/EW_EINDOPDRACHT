fb06dc96ac996b6949309ab5123d9bbbbdc48857
emileadam <emile_adam@hotmail.com>
2017-12-13 10:25:07 +0100

    test animate out

--- src/js/classes/WorldElement.js
+++ src/js/classes/WorldElement.js
@@ -21,14 +21,13 @@ export default class WorldElement {
     this._setupAnimations();
 
     this.animateGrowth();
-
-    this.wiggleAddition = 0.0025;
   }
 
   _constructMesh = () => {
     this.mesh = new THREE.Mesh(this.geom, this.mats);
     this.mesh.receiveShadow = true;
     this.mesh.castShadow = true;
+    // this.mesh.position.set(0, 0, 0);
     this.mesh.position.set(this.positionVector.x, this.positionVector.y, this.positionVector.z);
     console.log(this.mesh);
     this.mesh.scale.set(this.scaleFactor, this.scaleFactor, this.scaleFactor);
@@ -37,93 +36,74 @@ export default class WorldElement {
   _setupAnimations = () => {
     console.log(`SETUP ANIMATIONS`);
     this.mixer = new THREE.AnimationMixer(this.mesh);
-    //this.clips = this.mesh.geometry.animations;
     this.clock = new THREE.Clock();
 
-    //this.clips = THREE.AnimationClip.CreateClipsFromMorphTargetSequences(`animation_`, this.mesh.geometry.morphTargets, 24, true);
-
-    //this.growthClip = THREE.AnimationClip.findByName(this.clips, `animation_`);
-    this.growthClip = THREE.AnimationClip.CreateFromMorphTargetSequence(`animation_`, this.mesh.geometry.morphTargets, 60, true);
-
+    this.growMorphTargs = this.mesh.geometry.morphTargets.filter(morphTarget => morphTarget.name.startsWith(`grow_`));
+    this.growthClip = THREE.AnimationClip.CreateFromMorphTargetSequence(`grow_`, this.growMorphTargs, 60, true);
     this.growthAction = this.mixer.clipAction(this.growthClip);
-    //this.growthAction.repetitions = 0;
-    //NOTE: Dirty fix with LoopPingPong, trying to pause the ping ponged animation at the right time
+    console.log(this.growMorphTargs);
     this.growthAction.setLoop(THREE.LoopOnce);
     this.growthAction.clampWhenFinished = true;
 
-    // this.clip = this.mesh.geometry.animations[0];
-    // this.clip = THREE.AnimationClip.createFromMorphTargetSequence(this.geom.morphTargets, 30);
-    // this.mixer.clipAction(this.clip).setDuratation(5).play();
+    this.wiggleMorphTargs = this.mesh.geometry.morphTargets.filter(morphTarget => morphTarget.name.startsWith(`wiggle_`));
+    this.wiggleClip = THREE.AnimationClip.CreateFromMorphTargetSequence(`wiggle_`, this.wiggleMorphTargs, 60, false);
+    this.wiggleAction = this.mixer.clipAction(this.wiggleClip);
+    this.wiggleAction.setLoop(THREE.LoopRepeat);
+    this.wiggleAction.clampWhenFinished = false;
+
+    this.shrinkMorphTargs = this.mesh.geometry.morphTargets.filter(morphTarget => morphTarget.name.startsWith(`shrink_`));
+    this.shrinkClip = THREE.AnimationClip.CreateFromMorphTargetSequence(`shrink_`, this.shrinkMorphTargs, 60, true);
+    this.shrinkAction = this.mixer.clipAction(this.shrinkClip);
+    this.shrinkAction.setLoop(THREE.LoopOnce);
+    // this.shrinkAction.setLoop(THREE.LoopPingPong);
+    // this.shrinkAction.clampWhenFinished = true;
+
+    this.wiggled = false;
+    this.shrunk = false;
   }
 
   toggleMeshVisibility = () => this.mesh.visible = !this.mesh.visible;
 
   animateGrowth = () => {
-    // NOTE: adaption of best fix, causes element to snap away
-    //this.growthAction.reset();
-
     this.growthAction.play();
-    //this.growthAction.timeScale = 1;
-
-    window.requestAnimationFrame(() => this.updateAnimationMixer());
-
-    // this.scaleFactor += this.scaleFactorIncreasement;
-    // this.mesh.scale.set(this.scaleFactor, this.scaleFactor, this.scaleFactor);
-    //
-    // if (this.scaleFactor < 100) {
-    //   window.requestAnimationFrame(this.animateGrowth);
-    // }
+    this.updateAnimationMixer();
   }
+
+  animateWiggle = () => {
+    // this.growthAction.crossFadeTo(this.wiggleAction, 5, true);
+    // this.wiggleAction.play();
+
+    this.growthAction.crossFadeTo(this.wiggleAction, 5, true);
+    this.wiggleAction.play();
+    this.wiggled = true;
+  }
+
   animateShrink = () => {
+    console.log(`START ANIMATE SHRINK`);
 
-    // NOTE:
-    //Is normally best fix, does only work while animation action is running
-    //this.growthAction.play(); does not start animation action, because it only loops once
-    //and has already reached its final state
-    //this.growthAction.play();
-    //this.growthAction.timeScale = - 1;
-
-    // NOTE: adaption of best fix, causes element to snap away
-    // this.growthAction.paused = false;
-    // this.growthAction.play();
-    // this.growthAction.timeScale = - 1;
-
-    //NOTE: FOR DIRTY FIX WITH LoopPingPong
-    //this.growthAction.paused = false;
-
-    window.requestAnimationFrame(() => this.updateAnimationMixer());
-
-      // this.scaleFactor -= this.scaleFactorIncreasement;
-      // this.mesh.scale.set(this.scaleFactor, this.scaleFactor, this.scaleFactor);
-      // if (this.scaleFactor > 1) {
-      //   window.requestAnimationFrame(this.animateShrink);
-      // } else {
-      //   this.toggleMeshVisibility();
-      // }
+    // this.wiggleAction.effectiveWeight = 0;
+    //this.wiggleAction.crossFadeTo(this.shrinkAction, .5, true);
+    // this.wiggleAction.setLoop(THREE.LoopOnce);
+    this.wiggleAction.stop();
+    // this.wiggleAction.crossFadeTo(this.shrinkAction, .5, true);
+    // this.shrinkAction.play();
+    // this.shrunk = true;
   }
 
   updateAnimationMixer = () => {
     this.mixer.update(this.clock.getDelta());
 
-    if (this.growthAction.isRunning()) {
 
-      //NOTE: FOR DIRTY FIX WITH LoopPingPong
-      // if (parseFloat(this.growthAction.time).toFixed(1) === parseFloat(this.growthAction._clip.duration).toFixed(1) || parseFloat(this.growthAction.time).toFixed(1) === 0.1) {
-      //   console.log(parseFloat(this.growthAction.time).toFixed(1));
-      //   this.growthAction.paused = true;
-      // }
-
-      window.requestAnimationFrame(() => this.updateAnimationMixer());
+    if (!this.growthAction.isRunning() && !this.wiggleAction.isRunning() && !this.shrinkAction.isRunning() && this.wiggled === false) {
+      console.log(`START ANIMATE WIGGLE`);
+      this.animateWiggle();
     }
 
-    if (!this.mesh.visible) this.toggleMeshVisibility();
-  }
+    if (!this.mesh.visible || (this.shrunk && !this.shrinkAction.isRunning())) this.toggleMeshVisibility();
 
-  wiggle() {
-
-    this.mesh.rotation.x += this.wiggleAddition;
-
-    if (this.mesh.rotation.x > Math.PI / 12 || this.mesh.rotation.x < - Math.PI / 12) this.wiggleAddition = - this.wiggleAddition;
+    if (this.growthAction.isRunning() ||  this.wiggleAction.isRunning() || this.shrinkAction.isRunning()) {
+      window.requestAnimationFrame(() => this.updateAnimationMixer());
+    }
   }
 
 }
